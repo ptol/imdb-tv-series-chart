b2f33ed9b0346c09f1bcaca98cd9e411f7cbc72f
ptol <ptol>
2023-09-23 13:43:29 +0300

    0.8 new api fix

--- src/content/imdbApi.ts
+++ src/content/imdbApi.ts
@@ -1,23 +1,35 @@
-import {HtmlParser} from "./htmlParser"
 import {Episode, Season} from "./data"
 import {State} from "./state"
 import _ = require("lodash")
 
-const root = "https://www.imdb.com/title/"
+const root = "https://www.imdb.com/"
 
 export module ImdbApi {
 
-    export async function getSeasonData(number: number) {
-        const response = await fetch(root + `${State.movieId}/episodes/_ajax?season=${number}`)
-        const html = await response.text()
-        return {...HtmlParser.parseSeason(html), number}
+    export async function getSeasonData(buildId: string, number: number) {
+        const response = await fetch(
+        `${root}_next/data/${buildId}/title/${State.movieId}/episodes.json?season=${number}&tconst=${State.movieId}`)
+        const json = await response.json()
+        const episodesData: any[] = json.pageProps.contentData.section.episodes.items
+        const episodes = episodesData.map((x:any) => {
+            const date = x.releaseDate || {}
+            return {
+                url: `${root}title/${x.id}`,
+                title: x.titleText,
+                airDate: `${date.day}.${date.month}.${date.year}`,
+                rating: x.aggregateRating,
+                votes: x.voteCount
+            }
+        })
+        return {episodes, number}
     }
 
     export async function getAllSeasons() {
+        const buildId = JSON.parse(document.getElementById('__NEXT_DATA__')!.innerText).buildId
         const seasonsCount = State.seasonsCount
         const loadingCount = seasonsCount
         const seasons = await Promise.all(_.range(seasonsCount - loadingCount, seasonsCount).map(i =>
-            getSeasonData(i + 1)
+            getSeasonData(buildId, i + 1)
         ))
         let index = 0
         return seasons.map(data => {
